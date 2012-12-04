49d022a3e9dd9b019c3c6312458a5d7e603e1e09
Michael Howitz <mh@gocept.com>
2012-12-04 11:04:00 +0100

    Removed deform CSS as it is not needed for bootstrap.

--- js/deform_bootstrap/__init__.py
+++ js/deform_bootstrap/__init__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from fanstatic import Group
 from fanstatic import Library
 from fanstatic import Resource
 from js.bootstrap import bootstrap_js
 from js.chosen import chosen_jquery
-from js.deform import deform_css
 from js.deform import deform_js
 from js.deform import resource_mapping
 from pkg_resources import resource_filename
@@ -22,10 +20,8 @@ deform_bootstrap_js = Resource(
     "deform_bootstrap.js",
     depends=[deform_js, ])
 
-deform_bootstrap = Group([deform_bootstrap_js, deform_css])
-
 
 def includeme(config):
-    resource_mapping['deform'] = deform_bootstrap
+    resource_mapping['deform'] = deform_bootstrap_js
     resource_mapping['chosen'] = chosen_jquery
     resource_mapping['bootstrap'] = bootstrap_js
